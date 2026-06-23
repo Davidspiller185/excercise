17cc60e0c9433928d5a90ab1e1e6dda4afbc36da
david <dspiller321@gmail.com>
2026-06-23 17:13:18 +0300

    add exercise 38,41

--- aray_Mettod_EXE.js
+++ aray_Mettod_EXE.js
@@ -48,5 +48,45 @@ const chain = [1,2,3,4,5,6]
 const evens = chain.filter(num => num %2 === 0).map(num => num *2).reduce((acc,curent) => acc+curent,0)
 console.log(evens)
 
-// ת
+// תרגיל 38
+const item = [{ type: "fruit" }, { type: "veg" }, { type: "fruit" }]
+const itemReduse =item.reduce((acc,curent) => {
+const type = curent.type
+if (!(acc[item.type])){
+    acc[type] =[]
+} acc[type].push(curent)
+  return acc
 
+},{})
+console.log(itemReduse)
+
+// תרגיל 39
+ 
+const newArr = []
+function chekDuble(aray){
+    for (const num of aray){
+        if (newArr.includes(num)){ 
+         return "you have duble number"
+        }
+       else{
+         newArr.push(num)
+}
+    }
+    return "you have not duble number"
+}
+console.log(chekDuble([1,2,2,3,4,4,5,6]))
+
+// תרגיל 41
+const products = [
+
+  { name: "Laptop", price: 800, inStock: true,  category: "tech"    },
+
+  { name: "Phone",  price: 400, inStock: false, category: "tech"    },
+
+  { name: "Shirt",  price: 50,  inStock: true,  category: "fashion" },
+
+  { name: "Laptop",  price: 120,  inStock: true,  category: "tech" }
+
+];
+const result = products.filter(prod => prod.inStock === true && prod.price >100).sort((a,b) => a.price - b.price ).map(prod =>prod.name).join()
+console.log(result)
