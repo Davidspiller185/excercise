d3b72ed7624543908af77dddd7bcd1650edb6cb3
david <dspiller321@gmail.com>
2026-06-23 12:46:07 +0300

    add exercise 12,25

--- aray_Mettod_EXE.js
+++ aray_Mettod_EXE.js
@@ -23,3 +23,14 @@ const arr2 = [20,40,60]
 const dubble = arr2.reduce((acc,current) => acc * current, 1)
 console.log(dubble)
 
+// תרגיל 12
+const arrAge = [22,34,50,23,40]
+const total = arrAge.reduce((acc,curent) => acc + curent,0)
+console.log(total/arrAge.length)
+
+//תרגיל 25
+const arr3 = [30,40,50]
+const arrSort = arr3.sort((a,b) => b-a)
+console.log(arrSort)
+
+// תרגיל 29
