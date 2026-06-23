15993214386119e274f8c9acb713b471becb7e43
david <dspiller321@gmail.com>
2026-06-23 12:31:51 +0300

    add exercise 8,10

--- aray_Mettod_EXE.js
+++ aray_Mettod_EXE.js
@@ -12,3 +12,14 @@ const arr1 = [1,2,3,4,5,6]
 const arrfilter = arr1.filter(num =>num%2 == 0)
 console.log(arrfilter)
 
+// תרגיל 8
+const prices =[20,30,40,120,150,250]
+const pricesFilter = prices.filter(num => num>100)
+const pricesMap = pricesFilter.map(num => num * 1.1)
+console.log(pricesMap)
+
+// תרגיל 10
+const arr2 = [20,40,60]
+const dubble = arr2.reduce((acc,current) => acc * current, 1)
+console.log(dubble)
+
