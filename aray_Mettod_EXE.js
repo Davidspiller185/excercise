19bcdeebdd9157f2ce13db4dbaba2cb46abed0c9
david <dspiller321@gmail.com>
2026-06-23 12:15:11 +0300

    add exercise 2,4

---
+++ aray_Mettod_EXE.js
@@ -0,0 +1,14 @@
+
+
+// תרגיל 2
+const word = ["hello","world"]
+word.map((item,index) => word[index] = item.toUpperCase())
+console.log(word)
+
+
+
+// תרגיל 4
+const arr1 = [1,2,3,4,5,6]
+const arrfilter = arr1.filter(num =>num%2 == 0)
+console.log(arrfilter)
+
